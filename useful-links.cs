a29f62d8a94faddea5f8b444904bece708b5afb8
pauljamesb <37079324+pauljamesb@users.noreply.github.com>
2018-11-08 15:17:12 +0000

    Update useful-links.cs

--- useful-links.cs
+++ useful-links.cs
@@ -1,3 +1,7 @@
 // https://scottlilly.com/c-design-patterns-composition-over-inheritance/
 
 // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/knowing-when-to-use-override-and-new-keywords
+
+// https://docs.microsoft.com/en-us/dotnet/api/system.enum?view=netframework-4.7.2
+
+// https://docs.microsoft.com/en-us/dotnet/api/system.timespan?view=netframework-4.7.2
