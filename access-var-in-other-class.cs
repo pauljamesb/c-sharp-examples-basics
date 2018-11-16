7efed2fe9d65664c9d8285adb66f62dffd172923
pauljamesb <37079324+pauljamesb@users.noreply.github.com>
2018-11-16 13:30:54 +0000

    Create access-var-in-other-class.cs

---
+++ access-var-in-other-class.cs
@@ -0,0 +1,16 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string myString = Variables.Name;
+		Console.WriteLine(myString);
+	}
+}
+
+
+public class Variables
+{
+	public static string Name = "Test";
+}
